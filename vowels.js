f48aa4455ef29710510b1b4216f19fa552504af9
brysonstoner <35990406+brysonstoner@users.noreply.github.com>
2018-02-07 14:47:59 -0700

    Update vowels.js

--- vowels.js
+++ vowels.js
@@ -1,1 +1,7 @@
 //use tdd to write a javascript function to count the vowels in a string.
+
+var countVowels = function(string){
+    return string.match(/[aeiou]/gi).length;
+        }
+    
+module.exports = countVowels;
